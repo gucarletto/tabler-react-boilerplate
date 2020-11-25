745c2c1bf07f0e129e69d6bc11f8821b470e9b50
Gustavo Carletto <gucarletto@gmail.com>
2020-11-25 09:00:00 -0300

    Automatic logout on token expiration

--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -7,19 +7,28 @@ import {
 
 import { useAuth } from '../hooks/AuthContext';
 
+import moment from 'moment';
+
 interface RouteProps extends ReactDOMRouteProps {
   isPrivate?: boolean;
   component: React.ComponentType
 }
 
 const Route: React.FC<RouteProps> = ({ isPrivate = false, component: Component, ...rest }) => {
-  const { user } = useAuth();
+  const { store, expiration, signOut } = useAuth();
+
+  const date = moment();
+  const expirationDate = moment(expiration)
+  const expired = date.isAfter(expirationDate);
+  if(expired) {
+    signOut();
+  }
 
   return (
     <ReactDOMRoute
       { ...rest }
       render= {({ location }) => {
-        return isPrivate === !!user ? (
+        return isPrivate === !!store ? (
           <Component />
         ) : (
           <Redirect to={{
